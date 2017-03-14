37f552c017bfeb94ae7c028842e879fd25483844
Mike Schwab <mike.schwab@gmail.com>
2017-03-14 11:58:20 -0400

    attachment included in email

--- app/mailers/book_mailer.rb
+++ app/mailers/book_mailer.rb
@@ -1,9 +1,13 @@
 class BookMailer < ApplicationMailer
   def purchased purchase
+    uploader = PdfUploader.new Book.last
+    if Book.last.try(:pdf)
+      pdf = uploader.retrieve_from_store!(Book.last.pdf)
+      attachments['How to Enjoy Magic Cards.pdf'] = pdf
+    end
     mail :subject => "How to Enjoy Magic Cards (your purchased ebook)",
          :to      => purchase.email,
          :from    => ENV['DELIVERY_EMAIL']
-         # remember to include the attachment
   end
   
   def gifted
