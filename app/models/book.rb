bcb97002e3044022a61583b9b435f4a45c08fb1d
Mike Schwab <mike.schwab@gmail.com>
2017-06-26 16:13:23 -0400

    book knows its current edition

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,10 @@ class Book < ApplicationRecord
   has_many :editions
   mount_uploader :pdf, PdfUploader
   
+  def current_edition
+    editions.recent.published.first
+  end
+  
   def major_version
     version.split('.').first.to_i
   end
--- app/models/edition.rb
+++ app/models/edition.rb
@@ -3,6 +3,9 @@ class Edition < ApplicationRecord
   
   mount_uploader :pdf, PdfUploader
   
+  scope :recent, lambda { order(:major => :desc, :minor => :desc, :patch => :desc) }
+  scope :published, lambda { where Edition.arel_table[:release].lt DateTime.now }
+  
   def version
     "#{major}.#{minor}.#{patch}"
   end
