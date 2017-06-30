7f46a7a0467b1ed19aca4682ad7650be66c6eb3b
Mike Schwab <mike.schwab@gmail.com>
2017-06-30 06:42:49 -0400

    icon for editing table of contents

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,7 @@
 module BooksHelper
+  def focus_append_on table_of_contents
+    div_for table_of_contents, :focus_tool_for, :class => :focus_tool do
+      image_tag asset_path 'write.svg'
+    end
+  end
 end
