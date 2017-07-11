36ca2b415c8cf84cf621a11e21c75a7d6213ca91
Mike Schwab <mike.schwab@gmail.com>
2017-07-11 18:03:56 -0400

    typo in migration name

--- db/migrate/20170711205817_create_identifiers.rb
+++ db/migrate/20170711205817_create_identifiers.rb
@@ -1,4 +1,4 @@
-class CreateIdentitifiers < ActiveRecord::Migration[5.1]
+class CreateIdentifiers < ActiveRecord::Migration[5.1]
   def change
     create_table :identifiers do |t|
       t.string :provider
