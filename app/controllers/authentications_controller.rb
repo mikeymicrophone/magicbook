4a6af1bd0e23ade4c4fa08fb8bf8963013f01186
Mike Schwab <mike.schwab@gmail.com>
2017-07-11 19:03:55 -0400

    email in auth is within info hash

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -8,8 +8,8 @@ class AuthenticationsController < ApplicationController
     auth = request.env['omniauth.auth']
     if @identifier = Identifier.find_by(:provider => auth['provider'], :uid => auth['uid'])
       @magician = @identifier.magician
-    elsif @magician = Magician.find_by(:email => auth['email'])
-      @identity = @magician.identities.create :provider => auth['provider'], :uid => auth['uid'], :email => auth['email']
+    elsif @magician = Magician.find_by(:email => auth['info']['email'])
+      @identity = @magician.identities.create :provider => auth['provider'], :uid => auth['uid'], :email => auth['info']['email']
     end
     
     if @magician
