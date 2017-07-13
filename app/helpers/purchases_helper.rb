4001a31fd16633c82c11c2cdaa91eb1e182b0c76
Mike Schwab <mike.schwab@gmail.com>
2017-07-12 22:50:29 -0400

    purchase button on home page

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,2 +1,14 @@
 module PurchasesHelper
+  def wwemc_purchase
+    form_for Purchase.new do |f|
+      tag.script :src => 'https://checkout.stripe.com/checkout.js', :class => 'stripe-button', :data => {
+        :key => ENV['STRIPE_PUBLISHABLE_KEY'],
+        :amount => '200',
+        :name => 'Mikey Microphone',
+        :description => 'Ways We Enjoy Magic Cards',
+        :image => 'https://stripe.com/img/documentation/checkout/marketplace.png',
+        :locale => 'auto'
+      }
+    end
+  end
 end
