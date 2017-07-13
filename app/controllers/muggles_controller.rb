0550d7baf2c437d25ab41fef74605b202d837791
Mike Schwab <mike.schwab@gmail.com>
2017-07-13 00:57:22 -0400

    three rules to ensure invites are permissible

--- app/controllers/muggles_controller.rb
+++ app/controllers/muggles_controller.rb
@@ -4,10 +4,11 @@ class MugglesController < ApplicationController
   end
   
   def submit
+    @purchase = Purchase.find params[:purchase_id]
+    redirect_to root_url unless @purchase.magician == current_magician
     @muggle_1 = Muggle.create :email => params[:email_1], :purchase_id => params[:purchase_id], :magician => current_magician
     @muggle_2 = Muggle.create :email => params[:email_2], :purchase_id => params[:purchase_id], :magician => current_magician
     @muggle_3 = Muggle.create :email => params[:email_3], :purchase_id => params[:purchase_id], :magician => current_magician
     @muggle_4 = Muggle.create :email => params[:email_4], :purchase_id => params[:purchase_id], :magician => current_magician
-    byebug
   end
 end
--- app/models/muggle.rb
+++ app/models/muggle.rb
@@ -6,6 +6,7 @@ class Muggle < ApplicationRecord
          :recoverable, :rememberable, :trackable, :confirmable
          
   before_validation :set_password_to_card_name
+  validate :number_of_invited_muggles, :time_since_purchase
   
   def select_card_name
     card_name = ''
@@ -17,5 +18,13 @@ class Muggle < ApplicationRecord
   
   def set_password_to_card_name
     self.password = self.password_confirmation = select_card_name
+  end
+  
+  def number_of_invited_muggles
+    errors.add(:base, "Four muggles have already been shared with.  Please purchase another license to share with more.") if Muggle.where(:purchase_id => purchase_id).count > 3
+  end
+  
+  def time_since_purchase
+    errors.add(:base, "This purchase was made more than three days ago.") if purchase.created_at < 3.days.ago
   end
 end
