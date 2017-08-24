1bb13281a754aa129713fcca977ac49729c159e7
Mike Schwab <mike.schwab@gmail.com>
2017-08-23 20:18:54 -0400

    go to bookshelf after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     current_scribe || current_magician || current_muggle || "guest"
   end
+  
+  def after_sign_in_path_for user
+    books_path
+  end
 end
