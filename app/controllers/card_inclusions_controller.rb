b4cb3a98747bd40316a39518afa295e92bf3e296
Mike Schwab <mike.schwab@gmail.com>
2017-09-02 09:46:49 -0400

    unknown cards can be included in listed items

--- app/controllers/card_inclusions_controller.rb
+++ app/controllers/card_inclusions_controller.rb
@@ -6,6 +6,23 @@ class CardInclusionsController < ApplicationController
   
   def create
     @card = Card.find_by :name => card_inclusion_params[:card_name]
+    
+    unless @card
+      card = Card.new :name => card_inclusion_params[:card_name]
+      begin
+        open("https://api.scryfall.com/cards/named?exact=#{card_inclusion_params[:card_name]}") do |result|
+          card_data = JSON.parse result.read
+          card.multiverse_id = card_data['multiverse_id']
+          card.image_url = card_data['image_uris']['png']
+          card.save if card.image_url.present?
+          puts card.inspect
+        end
+        @card = card if card.persisted?
+      rescue OpenURI::HTTPError
+        puts "Not found: #{card_name}"
+      end
+    end
+    
     @card_inclusion = CardInclusion.new card_inclusion_params
     @card_inclusion.card = @card
     @card_inclusion.save if @card_inclusion.piece.magician == current_magician
