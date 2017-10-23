b0321882a9e3538827387de075775a551836a3d6
Mike Schwab <mike.schwab@gmail.com>
2017-10-22 20:50:11 -0400

    spec book version tracking

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Book, :type => :model do
+  describe 'version reporting' do
+    before do
+      @book = Fabricate :book
+    end
+    
+    it 'should know its major version' do
+      expect(@book.major_version).to be_kind_of Fixnum
+    end
+    
+    it 'should know its minor version' do
+      expect(@book.minor_version).to be_kind_of Fixnum
+    end
+    
+    it 'should know its patch version' do
+      expect(@book.patch_version).to be_kind_of Fixnum
+    end
+  end
+end
