79e4d8b71857d8f41eb860189c38c98d57e98b81
Mike Schwab <mike.schwab@gmail.com>
2023-10-17 11:37:22 -0400

    forms remote by default config

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Magicbook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
+    Rails.application.config.action_view.form_with_generates_remote_forms = true
 
     # Configuration for the application, engines, and railties goes here.
     #
