1d12c53b24b8b5fc1c9c4ccee7a3daf9092acbf1
Mike Schwab <mike.schwab@gmail.com>
2023-10-18 15:45:20 -0400

    reset_password_by_token is built-in now

--- app/models/magician.rb
+++ app/models/magician.rb
@@ -11,18 +11,18 @@ class Magician < ApplicationRecord
     encrypted_password.blank?
   end
   
-  def self.reset_password_by_token(attributes={})
-    recoverable = find_or_initialize_with_error_by(:reset_password_token, attributes[:reset_password_token])
-
-    if recoverable.persisted?
-      if recoverable.reset_password_period_valid?
-        recoverable.reset_password(attributes[:password], attributes[:password_confirmation])
-      else
-        recoverable.errors.add(:reset_password_token, :expired)
-      end
-    end
-
-    recoverable.reset_password_token = original_token if recoverable.reset_password_token.present?
-    recoverable
-  end
+ #  def self.reset_password_by_token(attributes={})
+ #    recoverable = find_or_initialize_with_error_by(:reset_password_token, attributes[:reset_password_token])
+ #
+ #    if recoverable.persisted?
+ #      if recoverable.reset_password_period_valid?
+ #        recoverable.reset_password(attributes[:password], attributes[:password_confirmation])
+ #      else
+ #        recoverable.errors.add(:reset_password_token, :expired)
+ #      end
+ #    end
+ #
+ #    recoverable.reset_password_token = original_token if recoverable.reset_password_token.present?
+ #    recoverable
+ #  end
 end
